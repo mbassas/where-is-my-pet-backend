e20076ed919e338c1f637d631a873f6d35b160fc
mbassas <montsebassas.mb@gmail.com>
2020-04-02 18:37:50 +0200

    FIX: CORS allowed origins fixed. PORT variable changed for development.

--- src/config.ts
+++ src/config.ts
@@ -2,10 +2,10 @@ const isProduction = process.env.NODE_ENV === "production";
 
 const Config = {
     PRODUCTION: isProduction,
-    PORT: process.env.PORT || 3000,
+    PORT: process.env.PORT || 3001,
     DATABASE_URL: process.env.DATABASE_URL,
     JWT_SECRET: process.env.JWT_SECRET || "jsonwebtokensupersecret",
-    ALLOWED_ORIGINS: isProduction ? "https://mbassas.github.io/where-is-my-pet-frontend/" : "*",
+    ALLOWED_ORIGINS: isProduction ? "https://mbassas.github.io" : "*",
     AES_PASSWORD: process.env.AES_PASSWORD || "password",
     AES_SALT: process.env.AES_SALT || "salt",
     HMAC_PASSWORD: process.env.HMAC_PASSWORD || "password"
